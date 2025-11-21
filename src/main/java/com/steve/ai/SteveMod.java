24a75b4e5733ace3ea1e4163fc94dd1761a49aa6
Iosif Pravets <chamaileontas@ya.ru>
2025-11-21 10:50:37 +0300

    Port SteveMod main class to 1.7.10 API

--- src/main/java/com/steve/ai/SteveMod.java
+++ src/main/java/com/steve/ai/SteveMod.java
@@ -1,72 +1,78 @@
 package com.steve.ai;
 
-import com.mojang.logging.LogUtils;
 import com.steve.ai.command.SteveCommands;
 import com.steve.ai.config.SteveConfig;
 import com.steve.ai.entity.SteveEntity;
 import com.steve.ai.entity.SteveManager;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod(SteveMod.MODID)
+@Mod(modid = SteveMod.MODID, name = SteveMod.NAME, version = SteveMod.VERSION)
 public class SteveMod {
     public static final String MODID = "steve";
-    public static final Logger LOGGER = LogUtils.getLogger();
-
-    public static final DeferredRegister<EntityType<?>> ENTITIES = 
-        DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MODID);
-
-    public static final RegistryObject<EntityType<SteveEntity>> STEVE_ENTITY = ENTITIES.register("steve",
-        () -> EntityType.Builder.of(SteveEntity::new, MobCategory.CREATURE)
-            .sized(0.6F, 1.8F)
-            .clientTrackingRange(10)
-            .build("steve"));
-
+    public static final String NAME = "Steve AI Mod";
+    public static final String VERSION = "1.0.0-mc1.7.10";
+    
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+    
+    @Instance(MODID)
+    public static SteveMod instance;
+    
     private static SteveManager steveManager;
-
-    public SteveMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ENTITIES.register(modEventBus);
-
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SteveConfig.SPEC);
-
-        modEventBus.addListener(this::commonSetup);
-        modEventBus.addListener(this::entityAttributes);
-
-        MinecraftForge.EVENT_BUS.register(this);
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOGGER.info("Steve AI Mod - Pre Initialization");
         
-        if (net.minecraftforge.fml.loading.FMLEnvironment.dist.isClient()) {
-            MinecraftForge.EVENT_BUS.register(com.steve.ai.client.SteveGUI.class);        }
+        // Load configuration
+        SteveConfig.init(event.getSuggestedConfigurationFile());
         
+        // Register Steve entity
+        int entityId = EntityRegistry.findGlobalUniqueEntityId();
+        EntityRegistry.registerGlobalEntityID(SteveEntity.class, "Steve", entityId);
+        EntityRegistry.registerModEntity(
+            SteveEntity.class,
+            "Steve",
+            entityId,
+            this,
+            80, // tracking range
+            3,  // update frequency
+            true // send velocity updates
+        );
+        
+        // Optional: Add spawning in world (commented out for manual spawning only)
+        // EntityRegistry.addSpawn(SteveEntity.class, 0, 0, 0, EnumCreatureType.creature, BiomeGenBase.getBiomeGenArray());
+        
+        LOGGER.info("Registered Steve entity with ID: " + entityId);
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        LOGGER.info("Steve AI Mod - Initialization");
+        
+        // Initialize Steve manager
         steveManager = new SteveManager();
+        
+        LOGGER.info("Steve AI Mod initialized successfully");
     }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {    }
-
-    private void entityAttributes(EntityAttributeCreationEvent event) {
-        event.put(STEVE_ENTITY.get(), SteveEntity.createAttributes().build());
+    
+    @EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        // Register commands
+        SteveCommands.register(event);
+        LOGGER.info("Registered Steve commands");
     }
-
-    @SubscribeEvent
-    public void onCommandRegister(RegisterCommandsEvent event) {        SteveCommands.register(event.getDispatcher());    }
-
+    
     public static SteveManager getSteveManager() {
         return steveManager;
     }
 }
-
